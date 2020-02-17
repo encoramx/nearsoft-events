55286fbe9950081b2ec6a47425e4bb5ab8e9a9f1
Axel Valdez <axel@paskola.com>
2020-02-17 15:39:28 -0700

    add filters

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 $(function () {
-  $('.filter').click(function (e) {
+  $('.filter.city').click(function (e) {
     e.preventDefault();
     $this = $(this);
-    $('body').attr('class', '').addClass($this.attr('href'));
+    $('body').attr('class', '');
+    if ($this.attr('href') !== 'all') {
+      $('body').addClass($this.attr('href')).addClass('show-city')
+    }
   });
+
+  $('.filter.type').click(function (e) {
+    e.preventDefault();
+    $this = $(this);
+    $('body').attr('class', '');
+    if ($this.attr('href') !== 'all') {
+      $('body').addClass($this.attr('href')).addClass('show-type')
+    }
+  });
+
 });
